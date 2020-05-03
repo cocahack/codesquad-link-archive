9adee09a5e82e8aff21f689bbaf65828eee64078
Jake <cocahack@naver.com>
2020-05-03 18:04:13 +0900

    spring 프로젝트 생성

---
+++ src/main/java/me/cocahack/links_api/LinksApiApplication.java
@@ -0,0 +1,13 @@
+package me.cocahack.links_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinksApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/cocahack/links_api/LinksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.cocahack.links_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
